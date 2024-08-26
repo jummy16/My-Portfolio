ad3135f13506ce9d05044be445abecbe9d323cfd
jummy16 <ogungbeolajumoke@gmail.com>
2024-08-26 16:14:04 +0100

    updated project page

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -5,7 +5,12 @@ import e_commerce from '../assets/e-commerce.png'
 import wedding from '../assets/wedding.png'
 import './project.css'
 
+
+
 const Project = () => {
+  const eCommerce = "https://github.com/jummy16/E-Commerce"
+  const youtubeClone = "https://github.com/jummy16/YouTube-Cloning"
+  const weddingWebsite = "https://github.com/jummy16/wedding-website"
   return (
     <div id='Projects' className='p-10 md:p-24 text-white'>
         <h1 className='text-2xl md:text-4xl text-white font-bold'>Projects</h1>
@@ -14,15 +19,22 @@ const Project = () => {
                 image={e_commerce}
                title="E-Commerce Site"
                 main="This is a E-Commerce site where you can buy and sell your product and the site 
-                  is created using Javascript and some other library"/>
+                  is created using Javascript and some other library"
+                  link = {eCommerce}
+                  />
+                  
           <ProjectCard
               image= {yt}
               title="Youtube Clone"
-              main="This is a fully functional youtube clone created using Vite React"/>
+              main="This is a fully functional youtube clone created using Vite React"
+              link = {youtubeClone}
+              />
           <ProjectCard
               image={wedding}
               title="Wedding Website"
-              main="A wedding website sample built with HTML, CSS and other library"/>
+              main="A wedding website sample built with HTML, CSS and other library"
+              link = {weddingWebsite}
+              />
         </div>
             <a href='https://github.com/jummy16?tab=repositories' target="_blank"><button className='project-btn duration-100 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>View More </button></a>
     </div>
--- src/Project/ProjectCard.jsx
+++ src/Project/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const ProjectCard = ({image,title, main}) => {
+const ProjectCard = ({image,title, main, link}) => {
   return (
     <div className='p-3 md:p-6 flex flex-col w-80 bg-[#0c0e29] shadow-xl shadow-slate-900 rounded-2xl'>
         <img className='p-4' src={image} alt=''/>
@@ -9,14 +9,11 @@ const ProjectCard = ({image,title, main}) => {
             {title}
         </h3>
         <p className='px-4 text-sm md:text-md leading-tight py-2'>{main}</p>
-        {/* <div className='mt-2 p-2 md:p-4 flex gap-2 md:gap-4'>
-            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-0 duration-100 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>
-                Demo
+        <div className='mt-2 p-2 md:p-4 flex gap-2 md:gap-4'>
+            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 duration-100 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>
+                <a href={link} target='_blank'>Source code </a>
             </button>
-            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-0 duration-100 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>
-               <a href='view-source:http://localhost:5174/video/27/GAH_9YWPm0Y'>Source code</a> 
-            </button>
-        </div> */}
+        </div>
     </div>
     
   )
