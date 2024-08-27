9f326f2e309ac3da4df4161b24b98d48777e9990
jummy16 <ogungbeolajumoke@gmail.com>
2024-08-27 10:25:31 +0100

    .

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
     const [menu, setMenu] = useState(false)
     const [showMenu, setShowMenu] = useState(true)
   return (
-    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20'>
+    <nav className='navbar flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20'>
         <span className='text-xl font-bold tracking-wide'>Portfolio</span>
-        <ul className={`${menu ? "block" :"hidden"}
-                mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none 
+        <ul className={`${menu ? "block" :"hidden"} 
+                 font-semibold md:mt-5 bg-black px-2  rounded-xl bg-opacity-30 md:border-none 
                 text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
             <a href='#About'><li className='text-md transition-all duration-300 p-1 md:p-0'>About</li></a>
             <a href='#Skills'><li className='text-md transition-all duration-300 p-1 md:p-0'>Skills</li></a>
